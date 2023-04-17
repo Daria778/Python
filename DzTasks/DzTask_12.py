87146ba20a8b38f98ee820501aa7b5a034d0ad68
Daria <shkatov.pad@gmail.com>
2023-04-17 18:15:36 +0300

    rer

---
+++ DzTasks/DzTask_12.py
@@ -0,0 +1,8 @@
+s = int(input('Enter the number s: '))
+p = int(input('Enter the number p: '))
+x = 1
+y = 1
+
+x = 1 - s + p                  #x^(2) - sx + p = 0
+y = s - x
+print(x, y)
---
+++ DzTasks/DzTask_14.py
@@ -0,0 +1,7 @@
+n = int(input('Enter the number: '))
+s = 1
+print(s)
+while s < n :
+    s = s * 2
+    if s < n :
+         print(s)
