9477096403fcd16bb8ed7f88ae049aa65fb839ae
Daria <shkatov.pad@gmail.com>
2023-04-19 17:09:17 +0300

    rewr

--- DzTasks/DzTask_16.py
+++ DzTasks/DzTask_16.py
@@ -1,1 +1,14 @@
+a = list()
+x = int(input('Enter the length of list: '))
+y = int(input('Enter the number you want to find: '))
+for i in range(x) :
+    z = int(input())
+    a.append(z)
+print(a)
 
+s = 0
+
+for i in range(len(a)) :
+    if a[i] == y :
+        s += 1
+print(s)
