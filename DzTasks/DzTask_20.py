e9f7989d5fdfb0d4a9611ccacd05f34ee35b46a2
Daria <shkatov.pad@gmail.com>
2023-04-20 20:55:24 +0300

    mice

--- DzTasks/DzTask_20.py
+++ DzTasks/DzTask_20.py
@@ -1,21 +1,25 @@
-s = str(input('Enter the word: '))
-s = list(s)
-s = len(s)
-d = 0
-z = 0
+s = str(input('Enter the word: ')).upper()
+print(s)
 
-a = [['a', 'e', 'i', 'o', 'u', 'l', 'n', 's', 't', 'r'], 
-     ['d', 'g'], 
-     ['b', 'c', 'm', 'p'],
-     [ 'f', 'h', 'v', 'w', 'y'], 
-     ['k'], 
-     ['j', 'x'], 
-     ['q', 'z']]
-a.append(s)
-print(a)
+a = {1 : "AEIOULNSTR", 2 : "DG", 3 : "BCMP", 4 : "FHVWY", 5 : "K", 8 : "JX", 10 : "QZ"} 
+z = "AEIOULNSTRDGBCMPFHVWYKJXQZ"
+count = 0
+b = {1 : "АВЕИНОРСТ", 2 : "ДКЛМПУ", 3 : "БГЁЬЯ", 4 : "ЙЫ", 5 : "ЖЗХЦЧ", 8 : "ШЭЮ", 10 : "ФЩЪ" }
+y = "АВЕИНОРСТДКЛМПУБГЁЬЯЙЫЖЗХЦЧШЭЮФЩЪ"
+if s[0] in z:
+    for i in s:
+        for k in a:
+            if i in a[k]:
+                count += k
+    print(count)
+elif s[0] in y:
+    for i in s:
+        for k in b:
+            if i in b[k]:
+                count += k
+    print(count)
+else:
+    print('Error')
 
-            
-     
-#сначала идем в список, потом в словарь, потом в список, берем значения, если одно из них равно значению из строки
-# то в сумму записываем значение ключей - словарей, печатаем ответ
-print(d)
+
+
