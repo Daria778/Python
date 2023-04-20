fc772ae44c6e16c4f818ccbba62e1130921b5970
Daria <shkatov.pad@gmail.com>
2023-04-20 20:56:52 +0300

    mice

--- DzTasks/DzTask_20.py
+++ DzTasks/DzTask_20.py
@@ -3,9 +3,12 @@ print(s)
 
 a = {1 : "AEIOULNSTR", 2 : "DG", 3 : "BCMP", 4 : "FHVWY", 5 : "K", 8 : "JX", 10 : "QZ"} 
 z = "AEIOULNSTRDGBCMPFHVWYKJXQZ"
+
 count = 0
+
 b = {1 : "АВЕИНОРСТ", 2 : "ДКЛМПУ", 3 : "БГЁЬЯ", 4 : "ЙЫ", 5 : "ЖЗХЦЧ", 8 : "ШЭЮ", 10 : "ФЩЪ" }
 y = "АВЕИНОРСТДКЛМПУБГЁЬЯЙЫЖЗХЦЧШЭЮФЩЪ"
+
 if s[0] in z:
     for i in s:
         for k in a:
