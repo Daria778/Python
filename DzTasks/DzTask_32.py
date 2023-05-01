b2a99aa9d258ca9a8946c9898eecc974242f8272
Daria <shkatov.pad@gmail.com>
2023-05-01 14:09:23 +0300

    oifd

---
+++ DzTasks/DzTask_32.py
@@ -0,0 +1,26 @@
+n = int(input('Enter the length of the list: '))
+a = list()
+
+for i in range(n) :
+    z = int(input())
+    a.append(z)
+print(a)
+
+b = int(input('Enter the lower restriction: '))
+c = int(input('Enter the upper restriction: '))
+
+d = list()
+g = list()
+for i in range(len(a)):
+    if a[i] >= b and a[i] <= c:
+        d.append(i)
+    else:
+        pass
+
+for i in range(len(a)):
+    if a[i] >= b and a[i] <= c:
+        g.append(a[i])
+    else:
+        pass
+print(d)
+print(g)
